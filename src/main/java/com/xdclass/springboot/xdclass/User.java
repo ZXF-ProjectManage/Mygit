cc2e55a5ec6ab03a6987224542d4d88a91ecdef6
ZhengXiaoFeng <1310598856@qq.com>
2022-05-16 16:32:11 +0800

    这是第一次提交

---
+++ src/main/java/com/xdclass/springboot/xdclass/User.java
@@ -0,0 +1,26 @@
+package com.xdclass.springboot.xdclass;
+
+import io.swagger.annotations.ApiParam;
+
+public class User {
+
+    @ApiParam(name = "name",value = "name",defaultValue = "")
+    public String name;
+    @ApiParam(name = "age",value = "age",defaultValue = "")
+    public String age;
+    @ApiParam(name = "sex",value = "sex",defaultValue = "")
+    public String sex;
+    @ApiParam(name = "birth",value = "birth",defaultValue = "")
+    public String birth;
+
+    public static void main(String[] args) {
+
+        String data="{\n" +
+                "  \"username\":\"xdclass\",\n" +
+                "  \"sex\":\"male\",\n" +
+                "  \"birth\":\"1991-11-11\"\n" +
+                "}";
+        String msg = "";
+    }
+
+}
